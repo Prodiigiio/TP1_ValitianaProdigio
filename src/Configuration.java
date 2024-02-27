dde6fcfda4cf65e879c12a76423c709f7453c6df
prodii <pvalitiana@gmail.com>
2024-02-27 10:33:29 -0500

    copie Profonde CONFIGURATION Ajouté une boucle qui passe par l'intérieur du composant et copier TOUT

--- src/Configuration.java
+++ src/Configuration.java
@@ -1,22 +1,31 @@
+import java.util.Arrays;
+
 public class Configuration {
     private String DESCRIPTION;
     private double prixMax;
     private Composant[] composants;
     private final int MAX_COMPOSANTS = 20;
 
-
-    public Configuration(String description, double prixMax, Composant[] composants){
+    public Configuration(String description, double prixMax, Composant[] composants) {
         setDescription(description);
         setPrixMax(prixMax);
         setComposants(composants);
     }
 
-    public Configuration(Configuration originale){
+    public Configuration(Configuration originale) {
         setDescription(originale.getDESCRIPTION());
         setPrixMax(originale.getPrixMax());
         setComposants(originale.getComposants());
-    }
 
+        for (int i = 0; i < getComposants().length; i++) {
+            if(getComposants()[i] != null){
+                getComposants()[i] = originale.getComposants()[i].copier();
+               // getNbComposants()[i] += composants[i].copier(); verifier plus tard
+            }
+        }
+
+
+    }
 
     public String getDESCRIPTION() {
         return DESCRIPTION;
@@ -35,14 +44,14 @@ public class Configuration {
     }
 
 
-
     public Composant[] getComposants() {
         return this.composants;
     }
 
     public void setComposants(Composant[] composants) {
-        if(composants.length > MAX_COMPOSANTS) //source pour "Guard clause": mon adelphe
+        if (composants.length > MAX_COMPOSANTS) {    //source pour "Guard clause": mon adelphe
             return;
+        }
         this.composants = tableauComposantRearrange(composants);
     }
 
@@ -60,27 +69,28 @@ public class Configuration {
         return coutTotalSansTaxes;
     }
 
-    public Composant rechercher(String categorie){
+    public Composant rechercher(String categorie) {
         for (int i = 0; i < getComposants().length; i++) {
-            if(getComposants()[i] != null && getComposants()[i].getCategorie().equalsIgnoreCase(categorie))
+            if (getComposants()[i] != null && getComposants()[i].getCategorie().equalsIgnoreCase(categorie))
                 return getComposants()[i];
         }
         return null;
     }
-    public int getNbComposants(){
+
+    public int getNbComposants() {
         int compteurComposant = 0;
         for (int i = 0; i < getComposants().length; i++) {
-            if(getComposants()[i] !=null)
+            if (getComposants()[i] != null)
                 compteurComposant++;
         }
         return compteurComposant;
     }
 
-    public Composant[] tableauComposantRearrange(Composant[] composant){
+    public Composant[] tableauComposantRearrange(Composant[] composant) {
         Composant[] tableauTemp = new Composant[MAX_COMPOSANTS];
         int j = 0;
         for (int i = 0; i < composant.length; i++) {
-            if(composant[i] != null){
+            if (composant[i] != null) {
                 tableauTemp[j] = composant[i];
                 j++;
             }
@@ -88,25 +98,26 @@ public class Configuration {
         return tableauTemp;
     }
 
-    public boolean ajouter(Composant composant){
+    public boolean ajouter(Composant composant) {
 
-        if(getNbComposants() >= MAX_COMPOSANTS) // guard clause source: mon adelphe
+        if (getNbComposants() >= MAX_COMPOSANTS) // guard clause source: mon adelphe
             return false;
 
         for (int i = 0; i < getComposants().length; i++) {
-            if(getComposants()[i] != null && getComposants()[i].getCategorie().equalsIgnoreCase(composant.getCategorie())){
+            if (getComposants()[i] != null && getComposants()[i].getCategorie().equalsIgnoreCase(composant.getCategorie())) {
                 System.out.println("Il y a déjà un composant de cette catégorie: " + getComposants()[i].toString());
                 return false;
             }
         }
 
-        if((getCoutTotalSansTaxes() + composant.getPrix()) > getPrixMax())
+        if ((getCoutTotalSansTaxes() + composant.getPrix()) > getPrixMax()) {
+            System.out.println("L'ajout de ce composant ferait dépasser le prix maximum: " + composant);
             return false;
-
+        }
 
         for (int i = 0; i < getComposants().length; i++) {
-            if(getComposants()[i] == null){
-                getComposants()[i] = composant.copier();
+            if (getComposants()[i] == null) {
+                getComposants()[i] = composant;
                 setComposants(tableauComposantRearrange(getComposants()));
                 System.out.printf("%s ajouté à la configuration (total=%.2f$)%n", getComposants()[i], getCoutTotalSansTaxes());
                 return true;
@@ -116,29 +127,25 @@ public class Configuration {
         return false;
     }
 
-
-
-    public boolean retirer(Composant composant){
+    public boolean retirer(Composant composant) {
         for (int i = 0; i < getComposants().length; i++) {
-            if(getComposants()[i] != null && getComposants()[i].estIdentique(composant)){
+            if (getComposants()[i] != null && getComposants()[i].estIdentique(composant)) {
+                System.out.printf("%s retiré de la configuration (total=%.2f$)%n", getComposants()[i], getCoutTotalSansTaxes());
                 getComposants()[i] = null;
                 setComposants(tableauComposantRearrange(getComposants()));
                 return true;
             }
-            if(getComposants()[i] != null && !(getComposants()[i].equals(composant))){
-                System.out.println("Composant introuvable: " + getComposants()[i].toString());
-                return false;
-            }
         }
-        setComposants(tableauComposantRearrange(getComposants()));
+        System.out.println("Composant introuvable: " + composant.toString());
         return false;
     }
 
-    public boolean remplacer(Composant composant){
+    public boolean remplacer(Composant composant) {
         for (int i = 0; i < getComposants().length; i++) {
-            if(getComposants()[i] != null && getComposants()[i].getCategorie().equalsIgnoreCase(composant.getCategorie())){
-                getComposants()[i] = null;
-                getComposants()[i] = composant;
+            if (composants[i] != null && composants[i].getCategorie().equalsIgnoreCase(composant.getCategorie().trim())) {
+                System.out.println(getComposants()[i].toString() + " retiré de la configuration");
+                composants[i] = composant;
+                System.out.println(getComposants()[i] + " ajouté de la configuration");
                 setComposants(tableauComposantRearrange(getComposants()));
                 return true;
             }
