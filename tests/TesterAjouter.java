e2dc4d8c1eed05859f9f4a57dfdee020bceec08f
prodii <pvalitiana@gmail.com>
2024-02-28 20:04:01 -0500

    TEST UNITAIRE - ajouterComposantMemeCategorieQuiMarchePas

--- tests/TesterAjouter.java
+++ tests/TesterAjouter.java
@@ -1,7 +1,6 @@
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TesterAjouter {
     @Test
@@ -12,6 +11,19 @@ public class TesterAjouter {
         boolean result = configuration.ajouter(composant);
 
         assertTrue(result);
+        assertEquals(1, configuration.getNbComposants());
+    }
+
+    @Test
+    void ajouterComposantMemeCategorieQuiMarchePas() {
+        Composant composant1 = new Composant("SSD", "Marque1", "Nom1", 100.0);
+        Composant composant2 = new Composant("SSD", "Marque2", "Nom2", 150.0);
+        Configuration configuration = new Configuration("Description", 500.0, new Composant[20]);
+        configuration.ajouter(composant1);
+
+        boolean result = configuration.ajouter(composant2);
+
+        assertFalse(result);
         assertEquals(1, configuration.getNbComposants());
     }
 }
